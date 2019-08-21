b8cb601aa610b4d5d77ba7b825ad670d8c1e5de4
brankbao <brankbao@webank.com>
2019-08-21 16:33:41 +0800

    #15 update doc

--- build/install.sh
+++ build/install.sh
@@ -15,14 +15,17 @@ fi
 source cmdb.cfg
 
 sed "s~{{CMDB_CORE_IMAGE_NAME}}~$cmdb_image_name~" docker-compose.tpl > docker-compose.yml
-sed -i "s~{{CMDB_CORE_EXTERNAL_PORT}}~$cmdb_server_port~" docker-compose.yml  
+sed -i "s~{{CMDB_SERVER_PORT}}~$cmdb_server_port~" docker-compose.yml  
 sed -i "s~{{CMDB_DATABASE_IMAGE_NAME}}~$database_image_name~" docker-compose.yml  
 sed -i "s~{{MYSQL_ROOT_PASSWORD}}~$database_init_password~" docker-compose.yml 
 sed -i "s~{{CAS_SERVER_URL}}~$cas_url~" docker-compose.yml
-sed -i "s~{{CMDB_CORE_EXTERNAL_IP}}~$cmdb_server_ip~" docker-compose.yml
+sed -i "s~{{CMDB_SERVER_IP}}~$cmdb_server_ip~" docker-compose.yml
 sed -i "s~{{CMDB_IP_WHITELISTS}}~$cmdb_ip_whitelists~" docker-compose.yml
- 
 
+
+docker-compose  -f docker-compose.yml  up -d
+
+ 
 
 
 
