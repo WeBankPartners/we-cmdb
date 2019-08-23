fd7bb966ef44c5de9329df61f59856e0bc331db4
Jun Chen <junchensz@foxmail.com>
2019-08-23 22:53:03 +0800

    update compose tpl

--- build/deploy_generate_compose.sh
+++ build/deploy_generate_compose.sh
@@ -7,4 +7,6 @@ sed -i "s~{{MYSQL_ROOT_PASSWORD}}~$database_init_password~" docker-compose.yml
 sed -i "s~{{CAS_SERVER_URL}}~$cas_url~" docker-compose.yml
 sed -i "s~{{CMDB_SERVER_IP}}~$cmdb_server_ip~" docker-compose.yml
 sed -i "s~{{CMDB_IP_WHITELISTS}}~$cmdb_ip_whitelists~" docker-compose.yml
-
+sed -i "s~{{CMDB_CORE_DATABASE_NAME}}~$cmdb_core_database_name~" docker-compose.yml
+sed -i "s~{{CMDB_CORE_DATABASE_USER_NAME}}~$cmdb_core_database_user_name~" docker-compose.yml
+sed -i "s~{{CMDB_CORE_DATABASE_PASSWORD}}~$cmdb_core_database_password~" docker-compose.yml
