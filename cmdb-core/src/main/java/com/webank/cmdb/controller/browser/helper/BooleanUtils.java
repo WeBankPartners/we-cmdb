b3096b5ffe841da77cb7b016d83c8ea040f5a81c
Jordan Zhang <jordanzhang@webank.com>
2019-08-30 14:38:57 +0800

    #61 Format code

--- cmdb-core/src/main/java/com/webank/cmdb/controller/browser/helper/BooleanUtils.java
+++ cmdb-core/src/main/java/com/webank/cmdb/controller/browser/helper/BooleanUtils.java
@@ -8,8 +8,9 @@ public class BooleanUtils {
 
     private static final Set<String> EXTENDED_WORDS_FOR_TRUE = Sets.newHashSet("是");
 
-    public static final boolean isTrue(String value){
-        if (EXTENDED_WORDS_FOR_TRUE.contains(value)) return true;
+    public static final boolean isTrue(String value) {
+        if (EXTENDED_WORDS_FOR_TRUE.contains(value))
+            return true;
         Boolean booleanObject = org.apache.commons.lang3.BooleanUtils.toBooleanObject(value);
         if (booleanObject == null) {
             return false;
@@ -19,6 +20,6 @@ public class BooleanUtils {
     }
 
     public static final <T> T derive(String condition, T trueResult, T failResult) {
-        return (isTrue(condition))? trueResult : failResult;
+        return (isTrue(condition)) ? trueResult : failResult;
     }
 }
