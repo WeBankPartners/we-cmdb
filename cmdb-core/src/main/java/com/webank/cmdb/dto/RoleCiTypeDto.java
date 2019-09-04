f55983b573892422c9235d4347888fc6b9840fa0
Jordan Zhang <jordanzhang@webank.com>
2019-09-04 10:02:25 +0800

    #61 Enhance the UI API

--- cmdb-core/src/main/java/com/webank/cmdb/dto/RoleCiTypeDto.java
+++ cmdb-core/src/main/java/com/webank/cmdb/dto/RoleCiTypeDto.java
@@ -1,5 +1,15 @@
 package com.webank.cmdb.dto;
 
+import static com.webank.cmdb.dto.CiTypePermissions.ACTION_CREATION;
+import static com.webank.cmdb.dto.CiTypePermissions.ACTION_ENQUIRY;
+import static com.webank.cmdb.dto.CiTypePermissions.ACTION_EXECUTION;
+import static com.webank.cmdb.dto.CiTypePermissions.ACTION_GRANT;
+import static com.webank.cmdb.dto.CiTypePermissions.ACTION_MODIFICATION;
+import static com.webank.cmdb.dto.CiTypePermissions.ACTION_REMOVAL;
+import static com.webank.cmdb.dto.CiTypePermissions.DISABLED;
+import static com.webank.cmdb.dto.CiTypePermissions.ENABLED;
+import static com.webank.cmdb.dto.CiTypePermissions.PARTIAL;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -7,12 +17,13 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.google.common.base.MoreObjects;
 import com.webank.cmdb.domain.AdmRoleCiType;
+import com.webank.cmdb.exception.CmdbException;
 import com.webank.cmdb.util.CollectionUtils;
 import com.webank.cmdb.util.DtoField;
 import com.webank.cmdb.util.DtoId;
 
 @JsonInclude(Include.NON_EMPTY)
-public class RoleCiTypeDto extends CiTypePermissions<RoleCiTypeDto, AdmRoleCiType> {
+public class RoleCiTypeDto extends BasicResourceDto<RoleCiTypeDto, AdmRoleCiType> {
     @DtoId
     @DtoField(domainField = "idAdmRoleCiType")
     private Integer roleCiTypeId;
@@ -21,12 +32,12 @@ public class RoleCiTypeDto extends CiTypePermissions<RoleCiTypeDto, AdmRoleCiTyp
     private Integer roleId;
     private String ciTypeName;
 
-    private String creationPermission;
-    private String removalPermission;
-    private String modificationPermission;
-    private String enquiryPermission;
-    private String executionPermission;
-    private String grantPermission;
+    private String creationPermission = DISABLED;
+    private String removalPermission = DISABLED;
+    private String modificationPermission = DISABLED;
+    private String enquiryPermission = DISABLED;
+    private String executionPermission = DISABLED;
+    private String grantPermission = DISABLED;
 
     @DtoField(domainField = "admCiType", updatable = false)
     private CiTypeDto ciType = new CiTypeDto();
@@ -96,6 +107,58 @@ public class RoleCiTypeDto extends CiTypePermissions<RoleCiTypeDto, AdmRoleCiTyp
             });
         }
         return dto;
+    }
+
+    public void enableActionPermission(String action) {
+        setActionPermission(action, ENABLED);
+    }
+
+    public void disableActionPermission(String action) {
+        setActionPermission(action, DISABLED);
+    }
+
+    public void enablePartialActionPermission(String action) {
+        setActionPermission(action, PARTIAL);
+    }
+
+    public boolean isActionPermissionEnabled(String action) {
+        return ENABLED.equalsIgnoreCase(getActionPermission(action));
+    }
+
+    private void setActionPermission(String action, String value) {
+        if (ACTION_CREATION.equalsIgnoreCase(action)) {
+            creationPermission = value;
+        } else if (ACTION_REMOVAL.equalsIgnoreCase(action)) {
+            removalPermission = value;
+        } else if (ACTION_MODIFICATION.equalsIgnoreCase(action)) {
+            modificationPermission = value;
+        } else if (ACTION_ENQUIRY.equalsIgnoreCase(action)) {
+            enquiryPermission = value;
+        } else if (ACTION_EXECUTION.equalsIgnoreCase(action)) {
+            executionPermission = value;
+        } else if (ACTION_GRANT.equalsIgnoreCase(action)) {
+            grantPermission = value;
+        } else {
+            throw new CmdbException("Unsupported action code: " + action);
+        }
+    }
+
+    private String getActionPermission(String action) {
+        if (ACTION_CREATION.equalsIgnoreCase(action)) {
+            return creationPermission;
+        } else if (ACTION_REMOVAL.equalsIgnoreCase(action)) {
+            return removalPermission;
+        } else if (ACTION_MODIFICATION.equalsIgnoreCase(action)) {
+            return modificationPermission;
+        } else if (ACTION_ENQUIRY.equalsIgnoreCase(action)) {
+            return enquiryPermission;
+        } else if (ACTION_EXECUTION.equalsIgnoreCase(action)) {
+            return executionPermission;
+        } else if (ACTION_GRANT.equalsIgnoreCase(action)) {
+            return grantPermission;
+        } else {
+            throw new CmdbException("Unsupported action code: " + action);
+        }
     }
 
     public Integer getRoleCiTypeId() {
