44cf95c9d8006604078a7ed5e3801245932147df
张森峰 <729346129@qq.com>
2019-09-20 11:25:53 +0800

    #214 ci data query graph optimization

--- cmdb-ui/src/pages/designing/ci-data.vue
+++ cmdb-ui/src/pages/designing/ci-data.vue
@@ -113,7 +113,7 @@ export default {
       const initEvent = () => {
         graph = d3.select("#graph");
         graph
-          .on("dblclick.zoom", null)
+          .on("click.zoom", null)
           .on("wheel.zoom", null)
           .on("mousewheel.zoom", null);
 
@@ -281,7 +281,7 @@ export default {
         .attr("stroke-opacity", ".2")
         .attr("fill", "#7f8fa6")
         .attr("fill-opacity", ".2");
-      d3.selectAll("text").attr("fill", "#000");
+      d3.selectAll(".edge text").attr("fill", "#7f8fa6");
     },
     colorNode(nodeName) {
       d3.selectAll('g[from="' + nodeName + '"] path')
@@ -307,7 +307,8 @@ export default {
       let nodesString = this.genDOT(data);
       this.loadImage(nodesString);
       this.graph.graphviz.renderDot(nodesString);
-      addEvent(".node", "click", async e => {
+      this.shadeAll();
+      addEvent(".node", "mouseover", async e => {
         e.preventDefault();
         e.stopPropagation();
         var g = e.currentTarget;
@@ -321,22 +322,13 @@ export default {
         d3.selectAll("g").attr("cursor", "pointer");
       });
 
-      addEvent("svg", "click", e => {
+      addEvent("svg", "mouseover", e => {
+        this.shadeAll();
         e.preventDefault();
         e.stopPropagation();
-        d3.selectAll("g path")
-          .attr("stroke", "#7f8fa6")
-          .attr("stroke-opacity", "1");
-        d3.selectAll("g polygon")
-          .attr("stroke", "#7f8fa6")
-          .attr("stroke-opacity", "1")
-          .attr("fill", "#7f8fa6")
-          .attr("fill-opacity", "1");
-
-        d3.selectAll("text").attr("fill", "#000");
       });
 
-      addEvent(".node", "dblclick", async e => {
+      addEvent(".node", "click", async e => {
         e.preventDefault();
         e.stopPropagation();
         var g = e.currentTarget;
