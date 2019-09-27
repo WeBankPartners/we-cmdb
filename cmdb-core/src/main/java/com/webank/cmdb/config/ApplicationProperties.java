24a50f9253b6e4367f4f7f6e3dbae876ab4cd27f
Jordan Zhang <jordanzhang@webank.com>
2019-09-27 17:17:27 +0800

    #194 Extract the code of view management from wecube platform

--- cmdb-core/src/main/java/com/webank/cmdb/config/ApplicationProperties.java
+++ cmdb-core/src/main/java/com/webank/cmdb/config/ApplicationProperties.java
@@ -21,10 +21,33 @@ public class ApplicationProperties {
     public class UIProperties {
         private Integer enumCategoryTypeSystem = 1;
         private Integer enumCategoryTypeCommon = 2;
+        private Integer ciTypeIdOfSystemDesign = 1;
+        private Integer ciTypeIdOfSubsystemDesign = 2;
+        private Integer ciTypeIdOfUnit = 8;
+        private Integer ciTypeIdOfSubsys = 7;
+        private Integer ciTypeIdOfHost = 12;
+        private Integer ciTypeIdOfInstance = 15;
+        private Integer ciTypeIdOfIdc = 16;
+        private Integer ciTypeIdOfZone = 17;
+        private Integer ciTypeIdOfZoneLink = 18;
+        private Integer ciTypeIdOfIdcDesign = 22;
+        private Integer ciTypeIdOfZoneDesign = 23;
+        private Integer ciTypeIdOfZoneLinkDesign = 24;
         private String enumCategoryCiTypeLayer = "ci_layer";
         private String enumCategoryCiTypeCatalog = "ci_catalog";
         private String enumCategoryCiTypeZoomLevels = "ci_zoom_level";
         private String statusAttributeName = "status";
+        private String referenceNameOfBelong = "属于";
+        private String referenceNameOfRunning = "运行在";
+        private String referenceNameOfRealize = "实现";
+        private String propertyNameOfState = "state";
+        private String enumCodeOfStateDelete = "delete";
+        private String propertyNameOfFixedDate = "fixed_date";
+        private String enumCategoryNameOfEnv = "env";
+        private String catNameOfArchitectureDesign = "tab_of_architecture_design";
+        private String catNameOfDeployDesign = "tab_of_deploy_design";
+        private String catNameOfQueryDeployDesign = "tab_query_of_deploy_design";
+        private String codeOfDeployDetail = "guid_of_deploy_detail";
     }
 
     @Data
---
+++ cmdb-core/src/main/java/com/webank/cmdb/controller/ui/helper/ResourceTreeDto.java
@@ -0,0 +1,28 @@
+package com.webank.cmdb.controller.ui.helper;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.webank.cmdb.dto.CiTypeAttrDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResourceTreeDto {
+    private Integer ciTypeId;
+    private String guid;
+    private String flag;
+    private Object attrs;
+    private Object data;
+
+    private List<CiTypeAttrDto> referenceByAttributesWithBelongType;
+
+    private List<ResourceTreeDto> children = new LinkedList<>();
+}
+
