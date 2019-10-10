54178fbe9c91561707b5993baa2ff8435e84fb0c
Jun Chen <junchensz@foxmail.com>
2019-10-10 15:43:56 +0800

    #283 add custom param for starting cmdb

--- build/start.sh
+++ build/start.sh
@@ -7,5 +7,5 @@ java -jar /application/cmdb-core.jar  --server.address=0.0.0.0 --server.port=${C
 --spring.datasource.password=${MYSQL_USER_PASSWORD}  \
 --cmdb.datasource.schema=${MYSQL_SERVER_DATABASE_NAME} \
 --cmdb.security.enabled=false \
---cmdb.security.whitelist-ip-address=${CMDB_IP_WHITELISTS}
+--cmdb.security.whitelist-ip-address=${CMDB_IP_WHITELISTS} \
 ${CUSTOM_PARAM} >>/log/cmdb-core.log 
