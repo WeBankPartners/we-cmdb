d44aa85f27c24caf99c426ea94052f486d0d144e
xuxuzhesi <ipqs45010@163.com>
2019-10-18 10:42:23 +0800

    English version

---
+++ cmdb-core/src/main/java/com/webank/cmdb/service/impl/AdmCodeValueService.java
@@ -0,0 +1,35 @@
+package com.webank.cmdb.service.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.webank.cmdb.domain.AdmCodeValue;
+import com.webank.cmdb.repository.CodeValueRepository;
+import com.webank.cmdb.util.SpringUtils;
+@Component
+public class AdmCodeValueService {
+    private static Map<String, String> codeValue = new HashMap<String, String>();
+   
+    @Autowired
+    private CodeValueRepository codeValueRepository;
+
+    public String getValueByCode(String Code) {
+        if (codeValue.size() < 1) {
+            getMap();
+        }
+        return codeValue.get(Code).toString();
+    }
+
+    private void getMap() {
+        CodeValueRepository bean = SpringUtils.getBean(CodeValueRepository.class);
+        @SuppressWarnings("unchecked")
+        List<AdmCodeValue> resultList = codeValueRepository.findAll();
+        resultList.forEach(c -> {
+            codeValue.put(c.getCode(), c.getValue());
+        });
+    }
+}
