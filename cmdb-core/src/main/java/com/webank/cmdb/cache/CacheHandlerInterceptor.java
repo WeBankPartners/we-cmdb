013ba3c7ea0b8a6564b6f134134f5ef053049c64
Jun Chen <junchensz@foxmail.com>
2019-10-18 15:50:04 +0800

    #284 registry the cache interceptor

--- cmdb-core/src/main/java/com/webank/cmdb/cache/CacheHandlerInterceptor.java
+++ cmdb-core/src/main/java/com/webank/cmdb/cache/CacheHandlerInterceptor.java
@@ -4,9 +4,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+@Component
 public class CacheHandlerInterceptor extends HandlerInterceptorAdapter{
 
     @Autowired
@@ -14,7 +16,6 @@ public class CacheHandlerInterceptor extends HandlerInterceptorAdapter{
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
         requestScopedCacheManager.clearCaches();
         return true;
     }
