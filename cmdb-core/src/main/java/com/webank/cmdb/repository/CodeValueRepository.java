bd1de0ae21d51922f742be7d08169fcf01e4d45a
xuxuzhesi <ipqs45010@163.com>
2019-10-18 18:59:36 +0800

    #316 Code review

--- cmdb-core/src/main/java/com/webank/cmdb/repository/CodeValueRepository.java
+++ cmdb-core/src/main/java/com/webank/cmdb/repository/CodeValueRepository.java
@@ -3,11 +3,8 @@ package com.webank.cmdb.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.webank.cmdb.domain.AdmCodeValue;
-
 public interface CodeValueRepository extends JpaRepository<AdmCodeValue, Integer>{
-    //@Query("SELECT *  FROM AdmCodeValue")
     List<AdmCodeValue> findAll();
 }
--- cmdb-core/src/main/java/com/webank/cmdb/util/SpringUtils.java
+++ cmdb-core/src/main/java/com/webank/cmdb/util/SpringUtils.java
@@ -14,25 +14,20 @@ public class SpringUtils implements ApplicationContextAware {
         if (SpringUtils.applicationContext == null) {
             SpringUtils.applicationContext = applicationContext;
         }
-        System.out.println("========ApplicationContext配置成功,在普通类可以通过调用SpringUtil.getAppContext()获取applicationContext对象,applicationContext=" + SpringUtils.applicationContext + "========");
     }
  
-    //获取applicationContext
     public static ApplicationContext getApplicationContext() {
         return applicationContext;
     }
  
-    //通过name获取 Bean.
     public static Object getBean(String name) {
         return getApplicationContext().getBean(name);
     }
  
-    //通过class获取Bean.
     public static <T> T getBean(Class<T> clazz) {
         return getApplicationContext().getBean(clazz);
     }
  
-    //通过name,以及Clazz返回指定的Bean
     public static <T> T getBean(String name, Class<T> clazz) {
         return getApplicationContext().getBean(name, clazz);
     }
