8321ffaeeb89a18db7a927d9e938e6b44f1bf0ed
Jordan Zhang <jordanzhang@webank.com>
2019-10-23 15:50:19 +0800

    #355 Adjust the context path for starting up cmdb

--- build/start.sh
+++ build/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p /log
-java -jar /application/cmdb-core.jar  --server.address=0.0.0.0 --server.port=${CMDB_SERVER_PORT} --server.servlet.context-path=/cmdb  \
+java -jar /application/cmdb-core.jar  --server.address=0.0.0.0 --server.port=${CMDB_SERVER_PORT} --server.servlet.context-path=/wecmdb  \
 --spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver \
 --spring.datasource.url=jdbc:mysql://${MYSQL_SERVER_ADDR}:${MYSQL_SERVER_PORT}/${MYSQL_SERVER_DATABASE_NAME}?serverTimezone=Asia\/Shanghai\&characterEncoding=utf8  \
 --spring.datasource.username=${MYSQL_USER_NAME} \
