7fada10418167b48a1c6dc03feaff3ad0a409daa
张森峰 <729346129@qq.com>
2019-10-25 15:53:07 +0800

    :green_heart: #364 update plugin router import type

--- cmdb-ui/src/router.js
+++ cmdb-ui/src/router.js
@@ -1,109 +1,152 @@
 import Vue from "vue";
 import Router from "vue-router";
+import changePassword from "@/pages/change-password";
+import permissions from "@/pages/admin/permission-management";
+import ciDesign from "@/pages/admin/cmdb-model-management";
+import enums from "@/pages/admin/enums";
+import ciData from "@/pages/designing/ci-data";
+import integrateQuery from "@/pages/designing/ci-integrated-query-execution";
+import integrateQueryMgmt from "@/pages/designing/ci-integrated-query-management";
+import queryLog from "@/pages/admin/query-log";
+import resetPassword from "@/pages/admin/resetPassword";
+import idcPlanningQuery from "@/pages/coming-soon";
+import idcResourcePlanningQuery from "@/pages/coming-soon";
+import applicationArchitectureQuery from "@/pages/coming-soon";
+import applicationDeploymentQuery from "@/pages/coming-soon";
+import idcPlanningDesign from "@/pages/designing/planning";
+import idcResourcePlanning from "@/pages/designing/resource-planning";
+import applicationArchitectureDesign from "@/pages/designing/application-architecture";
+import applicationDeploymentDesign from "@/pages/designing/application-deployment";
+
+const isPlugin = process.env.PLUGIN === "plugin";
 
 Vue.use(Router);
 export const pluginRouter = [
   {
     path: "setting/change-password",
     name: "changePassword",
-    component: () => import("@/pages/change-password")
+    component: () =>
+      isPlugin ? changePassword : import("@/pages/change-password")
   },
   {
     path: "/admin/permission-management",
     name: "permissions",
-    component: () => import("@/pages/admin/permission-management")
+    component: () =>
+      isPlugin ? permissions : import("@/pages/admin/permission-management")
   },
   {
     path: "/admin/cmdb-model-management",
     name: "ciDesign",
-    component: () => import("@/pages/admin/cmdb-model-management")
+    component: () =>
+      isPlugin ? ciDesign : import("@/pages/admin/cmdb-model-management")
   },
   {
     path: "/admin/base-data-management",
     name: "baseData",
-    component: () => import("@/pages/admin/enums")
+    component: () => (isPlugin ? enums : import("@/pages/admin/enums"))
   },
   {
     path: "/designing/enum-management",
     name: "enumManage",
-    component: () => import("@/pages/admin/enums")
+    component: () => (isPlugin ? enums : import("@/pages/admin/enums"))
   },
   {
     path: "/designing/enum-enquiry",
     name: "enumEnquiry",
-    component: () => import("@/pages/admin/enums")
+    component: () => (isPlugin ? enums : import("@/pages/admin/enums"))
   },
   {
     path: "/designing/ci-data-management",
     name: "ciDataManage",
-    component: () => import("@/pages/designing/ci-data")
+    component: () => (isPlugin ? ciData : import("@/pages/designing/ci-data"))
   },
   {
     path: "/designing/ci-data-enquiry",
     name: "ciDataEnquiry",
-    component: () => import("@/pages/designing/ci-data")
+    component: () => (isPlugin ? ciData : import("@/pages/designing/ci-data"))
   },
   {
     path: "/designing/ci-integrated-query-execution",
     name: "integrateQuery",
-    component: () => import("@/pages/designing/ci-integrated-query-execution")
+    component: () =>
+      isPlugin
+        ? integrateQuery
+        : import("@/pages/designing/ci-integrated-query-execution")
   },
   {
     path: "/designing/ci-integrated-query-management",
     name: "integrateQueryMgmt",
-    component: () => import("@/pages/designing/ci-integrated-query-management")
+    component: () =>
+      isPlugin
+        ? integrateQueryMgmt
+        : import("@/pages/designing/ci-integrated-query-management")
   },
   {
     path: "/admin/query-log",
     name: "queryLog",
-    component: () => import("@/pages/admin/query-log")
+    component: () => (isPlugin ? queryLog : import("@/pages/admin/query-log"))
   },
   {
     path: "/admin/user-password-management",
     name: "resetPassword",
-    component: () => import("@/pages/admin/resetPassword")
+    component: () =>
+      isPlugin ? resetPassword : import("@/pages/admin/resetPassword")
   },
   // 视图查询
   {
     path: "/view-query/idc-planning-query",
     name: "idcPlanningQuery",
-    component: () => import("@/pages/coming-soon")
+    component: () =>
+      isPlugin ? idcPlanningQuery : import("@/pages/coming-soon")
   },
   {
     path: "/view-query/idc-resource-planning-query",
     name: "idcResourcePlanningQuery",
-    component: () => import("@/pages/coming-soon")
+    component: () =>
+      isPlugin ? idcResourcePlanningQuery : import("@/pages/coming-soon")
   },
   {
     path: "/view-query/application-architecture-query",
     name: "applicationArchitectureQuery",
-    component: () => import("@/pages/coming-soon")
+    component: () =>
+      isPlugin ? applicationArchitectureQuery : import("@/pages/coming-soon")
   },
   {
     path: "/view-query/application-deployment-query",
     name: "applicationDeploymentQuery",
-    component: () => import("@/pages/coming-soon")
+    component: () =>
+      isPlugin ? applicationDeploymentQuery : import("@/pages/coming-soon")
   },
   // 视图管理
   {
     path: "/view-management/idc-planning-design",
     name: "idcPlanningDesign",
-    component: () => import("@/pages/designing/planning")
+    component: () =>
+      isPlugin ? idcPlanningDesign : import("@/pages/designing/planning")
   },
   {
     path: "/view-management/idc-resource-planning",
     name: "idcResourcePlanning",
-    component: () => import("@/pages/designing/resource-planning")
+    component: () =>
+      isPlugin
+        ? idcResourcePlanning
+        : import("@/pages/designing/resource-planning")
   },
   {
     path: "/view-management/application-architecture-design",
     name: "applicationArchitectureDesign",
-    component: () => import("@/pages/designing/application-architecture")
+    component: () =>
+      isPlugin
+        ? applicationArchitectureDesign
+        : import("@/pages/designing/application-architecture")
   },
   {
     path: "/view-management/application-deployment-design",
     name: "applicationDeploymentDesign",
-    component: () => import("@/pages/designing/application-deployment")
+    component: () =>
+      isPlugin
+        ? applicationDeploymentDesign
+        : import("@/pages/designing/application-deployment")
   }
 ];
 const appRouter = [
