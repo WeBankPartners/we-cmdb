62e034702583722a8c61806036cb04fefa34199b
Jordan Zhang <jordanzhang@webank.com>
2019-11-19 18:02:03 +0800

    #403 Fix unit tests

--- cmdb-core/src/test/java/com/webank/cmdb/controller/AbstractBaseControllerTest.java
+++ cmdb-core/src/test/java/com/webank/cmdb/controller/AbstractBaseControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,6 +25,7 @@ import com.webank.cmdb.util.CmdbThreadLocal;
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@WithMockUser(username="admin")
 public abstract class AbstractBaseControllerTest {
     @Autowired
     protected DataSource dataSource;
--- cmdb-core/src/test/java/com/webank/cmdb/controller/LegacyAbstractBaseControllerTest.java
+++ cmdb-core/src/test/java/com/webank/cmdb/controller/LegacyAbstractBaseControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,6 +24,7 @@ import com.webank.cmdb.service.DatabaseService;
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@WithMockUser(username="admin")
 public abstract class LegacyAbstractBaseControllerTest {
     @Autowired
     protected DataSource dataSource;
