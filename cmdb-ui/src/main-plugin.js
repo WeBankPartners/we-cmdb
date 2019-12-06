eec481ef25dd438af3be9d18d7966cea0d581048
zhangsenfeng <729346129@qq.com>
2019-12-06 14:18:56 +0800

    #474 use window.vm replace Vue

--- cmdb-ui/src/main-plugin.js
+++ cmdb-ui/src/main-plugin.js
@@ -1,11 +1,7 @@
-import Vue from "vue";
-import VueHighlightJS from "vue-highlight.js";
 import "vue-highlight.js/lib/allLanguages";
 import "highlight.js/styles/default.css";
 import router from "./router-plugin";
-import ViewUI from "view-design";
 import "view-design/dist/styles/iview.css";
-import locale from "view-design/dist/locale/en-US";
 import "./locale/i18n";
 import zh_CN from "./locale/i18n/zh-CN.json";
 import en_US from "./locale/i18n/en-US.json";
@@ -25,17 +21,8 @@ window.component("WeCMDBSimpleTable", WeCMDBSimpleTable);
 window.component("WeCMDBAttrInput", WeCMDBAttrInput);
 window.component("WeCMDBSequenceDiagram", WeCMDBSequenceDiagram);
 window.component("WeCMDBOrchestration", WeCMDBOrchestration);
-Vue.config.productionTip = false;
-
-Vue.use(ViewUI, {
-  transfer: true,
-  size: "default",
-  locale
-});
 
 window.locale("zh-CN", zh_CN);
 window.locale("en_US", en_US);
-
-Vue.use(VueHighlightJS);
 
 window.addRoutes && window.addRoutes(router, "cmdb");
