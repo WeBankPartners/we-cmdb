b38b816168bd5a63d2edf83d8e77f7983aa5662e
Jordan Zhang <jordanzhang@webank.com>
2019-12-17 09:50:45 +0800

    #511 Add ControllerAdvice for specific controller

--- cmdb-plugin/src/main/java/com/webank/plugins/wecmdb/controller/ApiController.java
+++ cmdb-plugin/src/main/java/com/webank/plugins/wecmdb/controller/ApiController.java
@@ -1,8 +1,5 @@
 package com.webank.plugins.wecmdb.controller;
 
-import static com.webank.plugins.wecmdb.dto.JsonResponse.okay;
-import static com.webank.plugins.wecmdb.dto.JsonResponse.okayWithData;
-
 import java.util.List;
 import java.util.Map;
 
@@ -15,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.webank.plugins.wecmdb.dto.JsonResponse;
+import com.webank.plugins.wecmdb.dto.EntityDto;
 import com.webank.plugins.wecmdb.dto.OperateCiDto;
 import com.webank.plugins.wecmdb.dto.OperateCiDtoInputs;
 import com.webank.plugins.wecmdb.dto.OperateCiJsonResponse;
@@ -29,27 +26,26 @@ public class ApiController {
 
     @GetMapping("/entities/{entity-name}")
     @ResponseBody
-    public JsonResponse retrieveCiData(@PathVariable(value = "entity-name") String entityName,
+    public Object retrieveCiData(@PathVariable(value = "entity-name") String entityName,
             @RequestParam(value = "filter", required = false) String filter,
             @RequestParam(value = "sorting", required = false) String sorting,
             @RequestParam(value = "select", required = false) String select) {
-        return okayWithData(adapterService.getCiDataWithConditions(entityName, filter, sorting, select));
+        return adapterService.getCiDataWithConditions(entityName, filter, sorting, select);
     }
 
     @PostMapping("/entities/{entity-name}/create")
-    public JsonResponse createCiData(@PathVariable("entity-name") String entityName, @RequestBody List<Map<String, Object>> request) {
-        return okayWithData(adapterService.createCiData(entityName, request));
+    public List<Map<String, Object>> createCiData(@PathVariable("entity-name") String entityName, @RequestBody List<Map<String, Object>> request) {
+        return adapterService.createCiData(entityName, request);
     }
 
     @PostMapping("/entities/{entity-name}/update")
-    public JsonResponse updateCiData(@PathVariable("entity-name") String entityName, @RequestBody List<Map<String, Object>> request) {
-        return okayWithData(adapterService.updateCiData(entityName, request));
+    public List<Map<String, Object>> updateCiData(@PathVariable("entity-name") String entityName, @RequestBody List<Map<String, Object>> request) {
+        return adapterService.updateCiData(entityName, request);
     }
 
     @PostMapping("/entities/{entity-name}/delete")
-    public JsonResponse deleteCiData(@PathVariable("entity-name") String entityName, @RequestBody List<Map<String, Object>> request) {
+    public void deleteCiData(@PathVariable("entity-name") String entityName, @RequestBody List<Map<String, Object>> request) {
         adapterService.deleteCiData(entityName, request);
-        return okay();
     }
 
     @PostMapping("/data/confirm")
@@ -68,7 +64,7 @@ public class ApiController {
 
     @GetMapping("/data-model")
     @ResponseBody
-    public JsonResponse getDataModel() {
-        return okayWithData(adapterService.getDataModel());
+    public List<EntityDto> getDataModel() {
+        return adapterService.getDataModel();
     }
 }
