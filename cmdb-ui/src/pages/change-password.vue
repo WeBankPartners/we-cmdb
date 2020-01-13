efa6727d01ba692b3077aff03986fd72d10b723d
张森峰 <729346129@qq.com>
2020-01-13 15:01:08 +0800

    :art: #623 rename axios params and format core

--- cmdb-ui/src/pages/change-password.vue
+++ cmdb-ui/src/pages/change-password.vue
@@ -44,73 +44,72 @@
 </template>
 
 <script>
-import { editPassword } from "@/api/server.js";
+import { editPassword } from '@/api/server.js'
 export default {
-  data() {
+  data () {
     return {
       formItem: {
-        oldPassword: "",
-        newPassword: "",
-        confimPassword: ""
+        oldPassword: '',
+        newPassword: '',
+        confimPassword: ''
       },
       ruleValidate: {
         oldPassword: [
-          { required: true, message: this.$t("old_password_input_placeholder") }
+          { required: true, message: this.$t('old_password_input_placeholder') }
         ],
         newPassword: [
-          { required: true, message: this.$t("new_password_input_placeholder") }
+          { required: true, message: this.$t('new_password_input_placeholder') }
         ],
         confimPassword: [
           {
             required: true,
-            message: this.$t("please_input_new_password_again")
+            message: this.$t('please_input_new_password_again')
           },
           { validator: this.checkNewPassword }
         ]
       }
-    };
-  },
-  methods: {
-    checkNewPassword(rule, value, callback) {
-      if (value !== this.formItem.newPassword) {
-        callback(new Error(this.$t("please_input_right_new_password")));
-      } else {
-        callback();
-      }
-    },
-    handleSubmit(name) {
-      this.$refs[name].validate(valid => {
-        if (valid) {
-          this.editPassword();
-        } else {
-          this.$Message.error("Fail!");
-          return;
-        }
-      });
-    },
-    async editPassword() {
-      const { statusCode, data, message } = await editPassword({
-        password: this.formItem.oldPassword,
-        newPassword: this.formItem.newPassword
-      });
-      if (statusCode === "OK") {
-        this.$Message.success(this.$t("reset_password_success"));
-        this.$router.push("/homepage");
-      }
-    },
-    handleReset(name) {
-      this.$refs[name].resetFields();
-    },
-    focusOnInput() {
-      this.$nextTick(() => {
-        this.$refs["oldPassword"].focus();
-      });
     }
   },
-  mounted() {
-    this.focusOnInput();
+  methods: {
+    checkNewPassword (rule, value, callback) {
+      if (value !== this.formItem.newPassword) {
+        callback(new Error(this.$t('please_input_right_new_password')))
+      } else {
+        callback()
+      }
+    },
+    handleSubmit (name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          this.editPassword()
+        } else {
+          this.$Message.error('Fail!')
+        }
+      })
+    },
+    async editPassword () {
+      const { statusCode } = await editPassword({
+        password: this.formItem.oldPassword,
+        newPassword: this.formItem.newPassword
+      })
+      if (statusCode === 'OK') {
+        this.$Message.success(this.$t('reset_password_success'))
+        this.$router.push('/homepage')
+      }
+    },
+    handleReset (name) {
+      this.$refs[name].resetFields()
+    },
+    focusOnInput () {
+      this.$nextTick(() => {
+        this.$refs['oldPassword'].focus()
+      })
+    }
+  },
+  mounted () {
+    this.focusOnInput()
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
