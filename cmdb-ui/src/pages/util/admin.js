b1849a6aa7671432427a07beec2d3730cc822997
irvinezhao <irvinezhao@gmail.com>
2020-01-14 15:23:01 +0800

    fix eslint issue

--- cmdb-ui/src/pages/util/admin.js
+++ cmdb-ui/src/pages/util/admin.js
@@ -1,30 +1,21 @@
-import {
-  addDataPermissionAction,
-  removeDataPermissionAction,
-  addCITypeAttrsAttr,
-  removeCITypeAttrsAttr
-} from "@/api/server.js";
-
 const getCiTypeEnabledAction = (name, actions) => {
   if (!actions) {
-    return false;
+    return false
   } else {
     if (actions.enabledActions) {
-      return !!actions.enabledActions.find(_ => _ === name);
+      return !!actions.enabledActions.find(_ => _ === name)
     }
   }
-};
+}
 
 const getCiTypeAttribute = (ciTypeAttributeId, attributePermissions) => {
   if (!attributePermissions) {
-    return [];
+    return []
   } else {
-    const ciTypeAttribute = attributePermissions.find(
-      _ => _.ciTypeAttributeId === ciTypeAttributeId
-    );
-    return ciTypeAttribute ? ciTypeAttribute.enabledOptions : [];
+    const ciTypeAttribute = attributePermissions.find(_ => _.ciTypeAttributeId === ciTypeAttributeId)
+    return ciTypeAttribute ? ciTypeAttribute.enabledOptions : []
   }
-};
+}
 
 export const formatDataPermissionTree = (
   checkable,
@@ -33,217 +24,212 @@ export const formatDataPermissionTree = (
   setCITypeAttrsAttr,
   selectedPermission = []
 ) => {
-  const tagsDefaultStyle = { border: "#515a61 1px dashed" };
+  const tagsDefaultStyle = { border: '#515a61 1px dashed' }
   const tagsDefaultProps = {
     checkable: checkable || false,
-    color: "success"
-  };
+    color: 'success'
+  }
   return data.ciPermissionEntryPoints.map(_ => {
-    const ciType = selectedPermission.find(c => _.ciTypeId === c.ciTypeId);
+    const ciType = selectedPermission.find(c => _.ciTypeId === c.ciTypeId)
     return {
       title: _.ciTypeName,
       id: _.ciTypeId,
       expand: true,
       render: (h, { root, node, data }) => {
         return h(
-          "div",
+          'div',
           {
             style: {
-              display: "inline-block",
-              width: "100%"
+              display: 'inline-block',
+              width: '100%'
             }
           },
           [
-            h("span", data.title),
+            h('span', data.title),
             h(
-              "div",
+              'div',
               {
                 style: {
-                  display: "inline-block",
-                  float: "right",
-                  marginRight: "32px"
+                  display: 'inline-block',
+                  float: 'right',
+                  marginRight: '32px'
                 }
               },
               [
                 h(
-                  "Tag",
+                  'Tag',
                   {
                     props: {
-                      name: "CREATE",
+                      name: 'CREATE',
                       ...tagsDefaultProps,
-                      checked: getCiTypeEnabledAction("CREATE", ciType)
+                      checked: getCiTypeEnabledAction('CREATE', ciType)
                     },
                     style: tagsDefaultStyle,
                     on: {
-                      "on-change": (checked, name) => {
-                        setPermissionAction(checked, name, data.id);
+                      'on-change': (checked, name) => {
+                        setPermissionAction(checked, name, data.id)
                       }
                     }
                   },
-                  this.$t("permission_management_data_creation")
+                  this.$t('permission_management_data_creation')
                 ),
                 h(
-                  "Tag",
+                  'Tag',
                   {
                     props: {
-                      name: "REMOVE",
+                      name: 'REMOVE',
                       ...tagsDefaultProps,
-                      checked: getCiTypeEnabledAction("REMOVE", ciType)
+                      checked: getCiTypeEnabledAction('REMOVE', ciType)
                     },
                     style: tagsDefaultStyle,
                     on: {
-                      "on-change": (checked, name) => {
-                        setPermissionAction(checked, name, data.id);
+                      'on-change': (checked, name) => {
+                        setPermissionAction(checked, name, data.id)
                       }
                     }
                   },
-                  this.$t("permission_management_data_removal")
+                  this.$t('permission_management_data_removal')
                 ),
                 h(
-                  "Tag",
+                  'Tag',
                   {
                     props: {
-                      name: "MODIFY",
+                      name: 'MODIFY',
                       ...tagsDefaultProps,
-                      checked: getCiTypeEnabledAction("MODIFY", ciType)
+                      checked: getCiTypeEnabledAction('MODIFY', ciType)
                     },
                     style: tagsDefaultStyle,
                     on: {
-                      "on-change": (checked, name) => {
-                        setPermissionAction(checked, name, data.id);
+                      'on-change': (checked, name) => {
+                        setPermissionAction(checked, name, data.id)
                       }
                     }
                   },
-                  this.$t("permission_management_data_modification")
+                  this.$t('permission_management_data_modification')
                 ),
                 h(
-                  "Tag",
+                  'Tag',
                   {
                     props: {
-                      name: "QUERY",
+                      name: 'QUERY',
                       ...tagsDefaultProps,
-                      checked: getCiTypeEnabledAction("QUERY", ciType)
+                      checked: getCiTypeEnabledAction('QUERY', ciType)
                     },
                     style: tagsDefaultStyle,
                     on: {
-                      "on-change": (checked, name) => {
-                        setPermissionAction(checked, name, data.id);
+                      'on-change': (checked, name) => {
+                        setPermissionAction(checked, name, data.id)
                       }
                     }
                   },
-                  this.$t("permission_management_data_enquiry")
+                  this.$t('permission_management_data_enquiry')
                 ),
                 h(
-                  "Tag",
+                  'Tag',
                   {
                     props: {
-                      name: "PERFORM",
+                      name: 'PERFORM',
                       ...tagsDefaultProps,
-                      checked: getCiTypeEnabledAction("PERFORM", ciType)
+                      checked: getCiTypeEnabledAction('PERFORM', ciType)
                     },
                     style: tagsDefaultStyle,
                     on: {
-                      "on-change": (checked, name) => {
-                        setPermissionAction(checked, name, data.id);
+                      'on-change': (checked, name) => {
+                        setPermissionAction(checked, name, data.id)
                       }
                     }
                   },
-                  this.$t("permission_management_data_execution")
+                  this.$t('permission_management_data_execution')
                 ),
                 h(
-                  "Tag",
+                  'Tag',
                   {
                     props: {
-                      name: "GRANT",
+                      name: 'GRANT',
                       ...tagsDefaultProps,
-                      checked: getCiTypeEnabledAction("GRANT", ciType)
+                      checked: getCiTypeEnabledAction('GRANT', ciType)
                     },
                     style: tagsDefaultStyle,
                     on: {
-                      "on-change": (checked, name) => {
-                        setPermissionAction(checked, name, data.id);
+                      'on-change': (checked, name) => {
+                        setPermissionAction(checked, name, data.id)
                       }
                     }
                   },
-                  this.$t("permission_management_data_empower")
+                  this.$t('permission_management_data_empower')
                 )
               ]
             )
           ]
-        );
+        )
       },
       children: _.attributeAuthenticationEntryPoints
         ? _.attributeAuthenticationEntryPoints.map(j => {
-            return {
-              title: j.ciTypeAttributeName,
-              id: j.ciTypeAttributeId,
-              expand: true,
-              render: (h, { root, node, data }) => {
-                return h(
-                  "div",
-                  {
-                    style: {
-                      display: "inline-block",
-                      width: "100%"
-                    }
-                  },
-                  [
-                    h("span", data.title),
-                    h(
-                      "div",
-                      {
-                        style: {
-                          display: "inline-block",
-                          float: "right",
-                          marginRight: "35px",
-                          maxWidth: "200px"
-                        }
-                      },
-                      [
-                        h(
-                          "Select",
-                          {
-                            props: {
-                              disabled: !checkable,
-                              multiple: true,
-                              size: "small",
-                              value: ciType
-                                ? getCiTypeAttribute(
-                                    data.id,
-                                    ciType.attributePermissions
-                                  )
-                                : []
-                            },
-                            style: {
-                              overflow: "hidden"
-                            },
-                            on: {
-                              "on-change": value => {
-                                setCITypeAttrsAttr(value, j.ciTypeAttributeId);
-                              }
-                            }
+          return {
+            title: j.ciTypeAttributeName,
+            id: j.ciTypeAttributeId,
+            expand: true,
+            render: (h, { root, node, data }) => {
+              return h(
+                'div',
+                {
+                  style: {
+                    display: 'inline-block',
+                    width: '100%'
+                  }
+                },
+                [
+                  h('span', data.title),
+                  h(
+                    'div',
+                    {
+                      style: {
+                        display: 'inline-block',
+                        float: 'right',
+                        marginRight: '35px',
+                        maxWidth: '200px'
+                      }
+                    },
+                    [
+                      h(
+                        'Select',
+                        {
+                          props: {
+                            disabled: !checkable,
+                            multiple: true,
+                            size: 'small',
+                            value: ciType ? getCiTypeAttribute(data.id, ciType.attributePermissions) : []
                           },
-                          j.options.map(o => {
-                            return h(
-                              "Option",
-                              {
-                                props: {
-                                  key: o.key,
-                                  value: o.key
-                                }
-                              },
-                              o.value
-                            );
-                          })
-                        )
-                      ]
-                    )
-                  ]
-                );
-              }
-            };
-          })
+                          style: {
+                            overflow: 'hidden'
+                          },
+                          on: {
+                            'on-change': value => {
+                              setCITypeAttrsAttr(value, j.ciTypeAttributeId)
+                            }
+                          }
+                        },
+                        j.options.map(o => {
+                          return h(
+                            'Option',
+                            {
+                              props: {
+                                key: o.key,
+                                value: o.key
+                              }
+                            },
+                            o.value
+                          )
+                        })
+                      )
+                    ]
+                  )
+                ]
+              )
+            }
+          }
+        })
         : []
-    };
-  });
-};
+    }
+  })
+}
