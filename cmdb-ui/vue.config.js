24462d786dcda51790d6b523e6fb23649b271648
张森峰 <729346129@qq.com>
2020-01-15 15:40:53 +0800

    #560 reconsitution params in component physical-graph

--- cmdb-ui/vue.config.js
+++ cmdb-ui/vue.config.js
@@ -1,4 +1,4 @@
-const CompressionPlugin = require("compression-webpack-plugin");
+const CompressionPlugin = require('compression-webpack-plugin')
 module.exports = {
   devServer: {
     // hot: true,
@@ -6,75 +6,75 @@ module.exports = {
     open: true,
     port: 3000,
     proxy: {
-      "/process": {
-        target: " http://localhost:9080"
+      '/process': {
+        target: 'http://localhost:9080'
       },
-      "/admin": {
-        target: " http://localhost:9080"
+      '/admin': {
+        target: 'http://localhost:9080'
       },
-      "/wecmdb": {
-        target: "http://localhost:9080"
+      '/wecmdb': {
+        target: 'http://localhost:9080'
       },
-      "/logout": {
-        target: "http://localhost:9080"
+      '/logout': {
+        target: 'http://localhost:9080'
       },
-      "/plugin": {
-        target: "http://localhost:9080"
+      '/plugin': {
+        target: 'http://localhost:9080'
       },
-      "/artifact": {
-        target: "http://localhost:9080"
+      '/artifact': {
+        target: 'http://localhost:9080'
       },
-      "/batch-job": {
-        target: "http://localhost:9080"
+      '/batch-job': {
+        target: 'http://localhost:9080'
       }
     }
   },
   runtimeCompiler: true,
-  publicPath: "/wecmdb/",
+  publicPath: '/wecmdb/',
   chainWebpack: config => {
-    if (process.env.PLUGIN !== "plugin") {
+    if (process.env.PLUGIN !== 'plugin') {
       // remove the old loader
-      const img = config.module.rule("images");
-      img.uses.clear();
+      const img = config.module.rule('images')
+      img.uses.clear()
       // add the new one
       img
-        .use("file-loader")
-        .loader("file-loader")
+        .use('file-loader')
+        .loader('file-loader')
         .options({
-          outputPath: "img"
-        });
+          outputPath: 'img'
+        })
     }
 
-    config.when(process.env.PLUGIN === "plugin", config => {
+    config.when(process.env.PLUGIN === 'plugin', config => {
       config
-        .entry("app")
+        .entry('app')
         .clear()
-        .add("./src/main-plugin.js"); //作为插件时
-    });
+        .add('./src/main-plugin.js') // 作为插件时
+    })
     config.when(!process.env.PLUGIN, config => {
       config
-        .entry("app")
+        .entry('app')
         .clear()
-        .add("./src/main.js"); //独立运行时
-    });
+        .add('./src/main.js') // 独立运行时
+    })
   },
-  productionSourceMap: process.env.PLUGIN !== "plugin",
+  productionSourceMap: process.env.PLUGIN !== 'plugin',
   configureWebpack: config => {
-    if (process.env.PLUGIN === "plugin") {
+    if (process.env.PLUGIN === 'plugin') {
       config.optimization.splitChunks = {}
-      return;
+      return
     }
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.NODE_ENV === 'production') {
       return {
         plugins: [
           new CompressionPlugin({
-            algorithm: "gzip",
-            test: /\.js$|\.html$|.\css/, //匹配文件名
-            threshold: 10240, //对超过10k的数据压缩
-            deleteOriginalAssets: false //不删除源文件
+            algorithm: 'gzip',
+            test: /\.js$|\.html$|.\css/, // 匹配文件名
+            threshold: 10240, // 对超过10k的数据压缩
+            deleteOriginalAssets: false // 不删除源文件
           })
         ]
-      };
+      }
     }
   }
-};
+}
