2883e694e8aae26f29abd2b6f5f2bf76031f6587
zhangsenfeng <729346129@qq.com>
2020-02-14 18:15:34 +0800

    :bug: #680 permission management select do not need to filter

--- cmdb-ui/src/pages/admin/permission-management.vue
+++ cmdb-ui/src/pages/admin/permission-management.vue
@@ -376,7 +376,8 @@ export default {
               ciType: { id: h.referenceId, name: h.name },
               type: 'text',
               ...components[h.inputType],
-              isMultiple: h.inputType === 'select'
+              isMultiple: h.inputType === 'select',
+              notFilte: true
             }
           })
           .concat(this.defaultColumns)
