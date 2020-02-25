b36e4d07e0665334515f63dd450f9305badfb161
zhangsenfeng <729346129@qq.com>
2020-02-25 09:41:06 +0800

    #525 ci-data password type cell

--- cmdb-ui/src/const/actions.js
+++ cmdb-ui/src/const/actions.js
@@ -33,7 +33,7 @@ export const components = {
     type: 'text'
   },
   password: {
-    component: 'WeCMDBCiPassword'
+    component: 'WeCMDBCIPassword'
   }
 }
 export const outerActions = [
--- cmdb-ui/src/main.js
+++ cmdb-ui/src/main.js
@@ -17,7 +17,7 @@ import WeCMDBSimpleTable from '../src/pages/components/simple-table.vue'
 import WeCMDBAttrInput from '../src/pages/components/attr-input'
 import WeCMDBSequenceDiagram from '../src/pages/components/sequence-diagram.vue'
 import WeCMDBOrchestration from '../src/pages/components/orchestration.vue'
-import WeCMDBCiPassword from '../src/pages/components/ci-password.vue'
+import WeCMDBCIPassword from '../src/pages/components/ci-password.vue'
 
 Vue.component('WeCMDBSelect', WeCMDBSelect)
 Vue.component('WeCMDBRefSelect', WeCMDBRefSelect)
@@ -26,7 +26,7 @@ Vue.component('WeCMDBSimpleTable', WeCMDBSimpleTable)
 Vue.component('WeCMDBAttrInput', WeCMDBAttrInput)
 Vue.component('WeCMDBSequenceDiagram', WeCMDBSequenceDiagram)
 Vue.component('WeCMDBOrchestration', WeCMDBOrchestration)
-Vue.component('WeCMDBCiPassword', WeCMDBCiPassword)
+Vue.component('WeCMDBCIPassword', WeCMDBCIPassword)
 Vue.config.productionTip = false
 
 Vue.use(ViewUI, {
