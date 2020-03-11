6f73eb2b9285eafa8ce386171b2a1f43bd0a0ef7
xuxuzhesi <ipqs45010@163.com>
2020-03-11 10:16:09 +0800

    #703 #713 plugin support filter query and expose service
    #703 #713 plugin support filter query and expose service

--- cmdb-plugin/src/main/java/com/webank/plugins/wecmdb/controller/WecubeAdapterController.java
+++ cmdb-plugin/src/main/java/com/webank/plugins/wecmdb/controller/WecubeAdapterController.java
@@ -1,10 +1,14 @@
 package com.webank.plugins.wecmdb.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.webank.cmdb.dto.QueryRequest;
+import com.webank.cmdb.util.BeanMapUtils;
+import com.webank.plugins.wecmdb.helper.HttpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,13 +25,31 @@ import com.webank.plugins.wecmdb.dto.wecube.OperateCiDtoInputs;
 import com.webank.plugins.wecmdb.dto.wecube.OperateCiJsonResponse;
 import com.webank.plugins.wecmdb.service.WecubeAdapterService;
 
+import javax.servlet.http.HttpServletRequest;
+
+
 @RestController
 public class WecubeAdapterController {
 
     @Autowired
     private WecubeAdapterService wecubeAdapterService;
+    @Autowired
+    private HttpServletRequest request;
 
-    @GetMapping("/entities/{entity-name}")
+
+    @PostMapping("/entities")
+    @ResponseBody
+    public Object retrieveCiDataSend(@RequestParam(value = "entity-name") String entityName,@RequestBody QueryRequest queryObject) throws Exception {
+        StringBuffer url=request.getRequestURL().append("/").append(entityName);
+        Map<String, Object> paramMap = BeanMapUtils.convertBeanToMap(queryObject);
+        try {
+            return HttpUtils.post(paramMap, url.toString());
+        } catch (IOException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @PostMapping("/entities/{entity-name}")
     @ResponseBody
     public Object retrieveCiData(@PathVariable(value = "entity-name") String entityName,@RequestBody QueryRequest queryObject) {
         return wecubeAdapterService.getCiDataWithConditions(entityName, queryObject);
@@ -38,14 +60,56 @@ public class WecubeAdapterController {
         return wecubeAdapterService.createCiData(entityName, request);
     }
 
+
+    @PostMapping("/entities/create")
+    @ResponseBody
+    public Object createCiDataSend(@RequestParam(value = "entity-name") String entityName, @RequestBody List<Map<String, Object>> cidata) {
+        StringBuffer url=request.getRequestURL().append("/entities").append("/").append(entityName).append("/create");
+        Map<String, Object> paramMap = BeanMapUtils.convertBeanToMap(cidata);
+        try {
+            return HttpUtils.post(paramMap, url.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     @PostMapping("/entities/{entity-name}/update")
     public List<Map<String, Object>> updateCiData(@PathVariable("entity-name") String entityName, @RequestBody List<Map<String, Object>> request) {
         return wecubeAdapterService.updateCiData(entityName, request);
     }
 
+
+
+    @PostMapping("/entities/update")
+    @ResponseBody
+    public Object updateCiDataSend(@RequestParam(value = "entity-name") String entityName, @RequestBody List<Map<String, Object>> cidata) {
+        StringBuffer url=request.getRequestURL().append("/entities").append("/").append(entityName).append("/update");
+        Map<String, Object> paramMap = BeanMapUtils.convertBeanToMap(cidata);
+        try {
+            return HttpUtils.post(paramMap, url.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     @PostMapping("/entities/{entity-name}/delete")
     public void deleteCiData(@PathVariable("entity-name") String entityName, @RequestBody List<Map<String, Object>> request) {
         wecubeAdapterService.deleteCiData(entityName, request);
+    }
+
+    @PostMapping("/entities/delete")
+    @ResponseBody
+    public Object deleteCiDataSend(@RequestParam(value = "entity-name") String entityName, @RequestBody List<Map<String, Object>> cidata) {
+        StringBuffer url=request.getRequestURL().append("/entities").append("/").append(entityName).append("/delete");
+        Map<String, Object> paramMap = BeanMapUtils.convertBeanToMap(cidata);
+        try {
+            return HttpUtils.post(paramMap, url.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @PostMapping("/data/confirm")
