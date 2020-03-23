bddecdf397ab2337c562b2857058feb9afcedff7
zhangsenfeng <729346129@qq.com>
2020-03-23 17:51:33 +0800

    #712 zoom level bug fixed

--- cmdb-ui/src/pages/util/event.js
+++ cmdb-ui/src/pages/util/event.js
@@ -1,12 +1,9 @@
 export const addEvent = (selector, type, fn) => {
   let nodesEl = document.querySelectorAll(selector)
   let length = nodesEl.length
-  const func = e => {
-    fn(e)
-  }
   for (let i = 0; i < length; i++) {
     let node = nodesEl[i]
-    node.removeEventListener(type, func)
-    node.addEventListener(type, func)
+    node.removeEventListener(type, fn)
+    node.addEventListener(type, fn)
   }
 }
