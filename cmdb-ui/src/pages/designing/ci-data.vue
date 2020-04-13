7a6d743ecbb4730a5a0266584808e831fe9247f9
elliotshi <elliotshi@outlook.com>
2020-04-13 18:10:36 +0800

    830 property typo

--- cmdb-ui/src/pages/designing/ci-data.vue
+++ cmdb-ui/src/pages/designing/ci-data.vue
@@ -62,7 +62,7 @@
       :title="$t('copyToNew')"
       @on-ok="handleCopyToNew"
       @on-cancel="copyVisible = false"
-      :mask-closablei="false"
+      :mask-closable="false"
     >
       <div class="copy-form">
         <div class="copy-label">{{ $t('input_set_of_copy') }}</div>
@@ -82,7 +82,7 @@
       @on-ok="handleCopySubmit"
       @on-cancel="copyTableVisible = false"
       :ok-text="$t('save')"
-      :mask-closablei="false"
+      :mask-closable="false"
     >
       <WeCMDBTable
         v-if="copyTableVisible"
