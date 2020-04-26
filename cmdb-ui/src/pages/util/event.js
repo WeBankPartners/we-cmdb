2118a100333d0dcb8310b55df6ce86980a3d2ab0
homiexie <624128114@qq.com>
2020-04-26 16:31:31 +0800

    #873 bug fix for model graph

--- cmdb-ui/src/pages/util/event.js
+++ cmdb-ui/src/pages/util/event.js
@@ -1,6 +1,5 @@
 export const addEvent = (selector, type, fn) => {
   let nodesEl = document.querySelectorAll(selector)
-  console.log(nodesEl)
   let length = nodesEl.length
   for (let i = 0; i < length; i++) {
     let node = nodesEl[i]
