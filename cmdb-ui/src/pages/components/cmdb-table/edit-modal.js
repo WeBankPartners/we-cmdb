b83050a5fba2ba515553a134196be42be5f41cb5
homiexie <624128114@qq.com>
2020-04-28 15:06:09 +0800

    #872 enhance for ci data

--- cmdb-ui/src/pages/components/cmdb-table/edit-modal.js
+++ cmdb-ui/src/pages/components/cmdb-table/edit-modal.js
@@ -251,12 +251,18 @@ export default {
           {this.modalVisible && (
             <div style={`width: ${this.tableWidth}px`}>
               {this.columns.map((column, index) => {
+                const d = {
+                  props: {
+                    'min-width': '130px',
+                    'max-width': '500px'
+                  }
+                }
                 return (
                   <div
                     style={`width:${WIDTH}px;display:inline-block;padding:5px;height: 30px;font-weight:600;background-color: #e8eaec`}
                     key={column.ciTypeAttrId || index}
                   >
-                    <Tooltip disabled={!column.description} content={column.description} placement="top">
+                    <Tooltip {...d} disabled={!column.description} content={column.description} placement="top">
                       {column.name || column.title}
                     </Tooltip>
                   </div>
