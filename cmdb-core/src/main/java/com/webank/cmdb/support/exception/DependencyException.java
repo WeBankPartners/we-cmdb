44b006880e4c48da5717b102b7959e9a49851be9
Jun Chen <junchensz@foxmail.com>
2020-06-04 09:35:42 +0800

    refactor code structure and filter rule

--- cmdb-core/src/main/java/com/webank/cmdb/support/exception/DependencyException.java
+++ cmdb-core/src/main/java/com/webank/cmdb/support/exception/DependencyException.java
@@ -1,4 +1,4 @@
-package com.webank.cmdb.exception;
+package com.webank.cmdb.support.exception;
 
 public class DependencyException extends CmdbException {
     private static final long serialVersionUID = 2829464363135608132L;
