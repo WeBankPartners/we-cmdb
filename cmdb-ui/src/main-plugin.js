4acbc10d3a82d6ba3d428993427c601a7061cb6f
zhangsenfeng <729346129@qq.com>
2020-06-10 14:38:38 +0800

    #907 remove useless files

--- cmdb-ui/src/main-plugin.js
+++ cmdb-ui/src/main-plugin.js
@@ -7,19 +7,17 @@ import enUS from './locale/i18n/en-US.json'
 import WeCMDBSelect from '../src/pages/components/select.vue'
 import WeCMDBRefSelect from './pages/components/ref-select.js'
 import WeCMDBRadioRroup from '../src/pages/components/radio-group.vue'
-import WeCMDBTable from './pages/components/table.js'
 import CMDBTable from '../src/pages/components/cmdb-table/index.js'
 import WeCMDBSimpleTable from '../src/pages/components/simple-table.vue'
 import WeCMDBAttrInput from '../src/pages/components/attr-input'
 import WeCMDBSequenceDiagram from '../src/pages/components/sequence-diagram.vue'
 import WeCMDBOrchestration from '../src/pages/components/orchestration.vue'
 import WeCMDBCIPassword from '../src/pages/components/ci-password.vue'
-import CMDBPermissionFilters from '../src/pages/components/filters'
+import CMDBPermissionFilters from '../src/pages/components/permission-filters'
 
 window.component('WeCMDBSelect', WeCMDBSelect)
 window.component('WeCMDBRefSelect', WeCMDBRefSelect)
 window.component('WeCMDBRadioRroup', WeCMDBRadioRroup)
-window.component('WeCMDBTable', WeCMDBTable)
 window.component('CMDBTable', CMDBTable)
 window.component('WeCMDBSimpleTable', WeCMDBSimpleTable)
 window.component('WeCMDBAttrInput', WeCMDBAttrInput)
