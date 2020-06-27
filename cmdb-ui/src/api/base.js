dac5986226a975ef8dd8a0fc8b209875025f114f
pobu168 <244202969@qq.com>
2020-06-27 17:10:19 +0800

    #942 Data center desin i18n

--- cmdb-ui/src/api/base.js
+++ cmdb-ui/src/api/base.js
@@ -1,4 +1,3 @@
-import { setCookie, getCookie } from '../pages/util/cookie.js'
 import Vue from 'vue'
 import axios from 'axios'
 export const baseURL = '/wecmdb/ui/v2'
@@ -7,62 +6,6 @@ export const req = axios.create({
   baseURL,
   timeout: 50000
 })
-let refreshRequest = null
-req.interceptors.request.use(
-  config => {
-    return new Promise((resolve, reject) => {
-      const currentTime = new Date().getTime()
-      const accessToken = getCookie('accessToken')
-      if (accessToken && config.url !== '/auth/v1/api/login') {
-        const expiration = getCookie('accessTokenExpirationTime') * 1 - currentTime
-        if (expiration < 1 * 60 * 1000 && !refreshRequest) {
-          refreshRequest = axios.get('/auth/v1/api/token', {
-            headers: {
-              Authorization: 'Bearer ' + getCookie('refreshToken')
-            }
-          })
-          refreshRequest.then(
-            res => {
-              setCookie(res.data.data)
-              config.headers.Authorization = 'Bearer ' + res.data.data.find(t => t.tokenType === 'accessToken').token
-              refreshRequest = null
-              resolve(config)
-            },
-            // eslint-disable-next-line handle-callback-err
-            err => {
-              refreshRequest = null
-              window.location.href = window.location.origin + window.location.pathname + '#/login'
-            }
-          )
-        }
-        if (expiration < 1 * 60 * 1000 && refreshRequest) {
-          refreshRequest.then(
-            res => {
-              setCookie(res.data.data)
-              config.headers.Authorization = 'Bearer ' + res.data.data.find(t => t.tokenType === 'accessToken').token
-              refreshRequest = null
-              resolve(config)
-            },
-            // eslint-disable-next-line handle-callback-err
-            err => {
-              refreshRequest = null
-              window.location.href = window.location.origin + window.location.pathname + '#/login'
-            }
-          )
-        }
-        if (expiration > 1 * 60 * 1000) {
-          config.headers.Authorization = 'Bearer ' + accessToken
-          resolve(config)
-        }
-      } else {
-        resolve(config)
-      }
-    })
-  },
-  error => {
-    return Promise.reject(error)
-  }
-)
 
 const throwError = res => new Error(res.message || 'error')
 req.interceptors.response.use(
