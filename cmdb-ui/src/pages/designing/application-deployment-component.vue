b22d95752843a2a56adc83ab34f8bb84f18a43a7
pobu168 <244202969@qq.com>
2020-06-29 16:02:25 +0800

    #942 Upgrade graph display

--- cmdb-ui/src/pages/designing/application-deployment-component.vue
+++ cmdb-ui/src/pages/designing/application-deployment-component.vue
@@ -306,10 +306,10 @@ export default {
           .on('dblclick.zoom', null)
           .on('wheel.zoom', null)
           .on('mousewheel.zoom', null)
-        const width = (window.innerWidth / 24) * 18
+        const width = ((window.innerWidth - 60) / 24) * 16 - 40
         this.graph.graphviz = graph
           .graphviz()
-          .width(width - 80)
+          .width(width)
           .height(window.innerHeight - 260)
           .zoom(true)
           .fit(true)
