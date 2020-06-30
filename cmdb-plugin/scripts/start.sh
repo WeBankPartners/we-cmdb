9ee6b9854954cae9ecc0d082928fe98bddb220f8
TTTTifura <13682559741@163.com>
2020-06-30 15:04:14 +0800

    #987 modify the log folder

--- cmdb-plugin/scripts/start.sh
+++ cmdb-plugin/scripts/start.sh
@@ -11,4 +11,4 @@ java -Duser.timezone=Asia/Shanghai -jar /application/wecube-plugins-wecmdb.jar  
 --spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver \
 --spring.datasource.url="jdbc:mysql://${db_host}:${db_port}/${db_schema}?characterEncoding=utf8&serverTimezone=Asia/Shanghai" \
 --spring.datasource.username=$4 \
---spring.datasource.password=$5 >>/log/wecube-plugins-wecmdb.log 
+--spring.datasource.password=$5 >>/data/wecmdb/log/cmdb-core.log
