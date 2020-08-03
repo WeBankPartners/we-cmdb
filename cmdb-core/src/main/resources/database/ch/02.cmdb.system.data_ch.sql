28b39bbecba783d27bdd99b9cc72ec773f721fc4
Jun Chen <junchensz@foxmail.com>
2020-08-03 17:24:28 +0800

    fix init sql

--- cmdb-core/src/main/resources/database/ch/02.cmdb.system.data_ch.sql
+++ cmdb-core/src/main/resources/database/ch/02.cmdb.system.data_ch.sql
@@ -1,13 +1,13 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping data for table wecmdb_bk.adm_attr_group: ~0 rows (approximately)
 /*!40000 ALTER TABLE `adm_attr_group` DISABLE KEYS */;
 /*!40000 ALTER TABLE `adm_attr_group` ENABLE KEYS */;
 
+SET FOREIGN_KEY_CHECKS=0;
 -- Dumping data for table wecmdb_bk.adm_basekey_cat: ~26 rows (approximately)
 /*!40000 ALTER TABLE `adm_basekey_cat` DISABLE KEYS */;
 INSERT INTO `adm_basekey_cat` (`id_adm_basekey_cat`, `cat_name`, `description`, `id_adm_role`, `id_adm_basekey_cat_type`, `group_type_id`) VALUES
@@ -815,5 +815,5 @@ INSERT INTO `adm_user` (`id_adm_user`, `name`, `code`, `encrypted_password`, `de
 /*!40000 ALTER TABLE `adm_user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+SET FOREIGN_KEY_CHECKS=1;
