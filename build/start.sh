1cfa8928c168b0cdf60c3140a6a3ea07ae6e5972
Jun Chen <junchensz@foxmail.com>
2020-08-19 16:03:30 +0800

    improve ci query performance and add performance measure log

--- build/start.sh
+++ build/start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 mkdir -p /log
-java -jar /application/cmdb-core.jar  --server.address=0.0.0.0 --server.port=${CMDB_SERVER_PORT} --server.servlet.context-path=/wecmdb  \
+
+JAVA_OPT="-verbose:gc -Xloggc:/data/wecmdb/log/wecmdb_gc_%p.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintAdaptiveSizePolicy"
+JAVA_OPT="${JAVA_OPT} -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=30m"
+
+java -jar /application/cmdb-core.jar ${JAVA_OPT} --server.address=0.0.0.0 --server.port=${CMDB_SERVER_PORT} --server.servlet.context-path=/wecmdb  \
 --spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver \
 --spring.datasource.url=jdbc:mysql://${MYSQL_SERVER_ADDR}:${MYSQL_SERVER_PORT}/${MYSQL_SERVER_DATABASE_NAME}?serverTimezone=Asia\/Shanghai\&characterEncoding=utf8  \
 --spring.datasource.username=${MYSQL_USER_NAME} \
@@ -8,4 +12,4 @@ java -jar /application/cmdb-core.jar  --server.address=0.0.0.0 --server.port=${C
 --cmdb.datasource.schema=${MYSQL_SERVER_DATABASE_NAME} \
 --cmdb.security.enabled=false \
 --cmdb.security.whitelist-ip-address=${CMDB_IP_WHITELISTS} \
-${CUSTOM_PARAM} >>/log/cmdb-core.log 
+${CUSTOM_PARAM} >>/data/wecmdb/log/wecmdb-core.log
