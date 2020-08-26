8d5cab03d93a93cd78c01be697a0087bff8cf421
Jun Chen <junchensz@foxmail.com>
2020-08-26 11:20:24 +0800

    use G1 gc strategy

--- cmdb-plugin/scripts/start.sh
+++ cmdb-plugin/scripts/start.sh
@@ -4,12 +4,12 @@ db_host=$1
 db_port=$2
 db_schema=$3
 
-JAVA_OPT="-Xmx3G -Xms512m "
+JAVA_OPT="-Xmx3G -Xms512m -XX:+UseG1GC -XX:MaxGCPauseMillis=200  -XX:MaxTenuringThreshold=14"
 JAVA_OPT="${JAVA_OPT} -verbose:gc -Xloggc:/data/wecmdb/log/wecmdb_gc_%p.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintAdaptiveSizePolicy"
 JAVA_OPT="${JAVA_OPT} -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=30m"
 
 mkdir -p /data/wecmdb/log
-java -Duser.timezone=Asia/Shanghai -jar /application/wecube-plugins-wecmdb.jar  ${JAVA_OPT} \
+java ${JAVA_OPT} -Duser.timezone=Asia/Shanghai -jar /application/wecube-plugins-wecmdb.jar  \
 --server.address=0.0.0.0 \
 --server.port=8081 \
 --spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver \
