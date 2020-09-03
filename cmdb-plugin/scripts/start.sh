8434cbe54169e761c8ad2adec08bdf099430de55
gavin2lee <gavin2lee@163.com>
2020-09-03 14:08:26 +0800

    #1076 refactor jwt signing key

--- cmdb-plugin/scripts/start.sh
+++ cmdb-plugin/scripts/start.sh
@@ -22,5 +22,4 @@ java ${JAVA_OPT} -Duser.timezone=Asia/Shanghai \
 --spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver \
 --spring.datasource.url="jdbc:mysql://${db_host}:${db_port}/${db_schema}?characterEncoding=utf8&serverTimezone=Asia/Shanghai" \
 --spring.datasource.username=$4 \
---spring.datasource.password=$5 \
---cmdb.security.jwt-signing-key=$6 >>/data/wecmdb/log/wecmdb-core.log
+--spring.datasource.password=$5 >>/data/wecmdb/log/wecmdb-core.log
