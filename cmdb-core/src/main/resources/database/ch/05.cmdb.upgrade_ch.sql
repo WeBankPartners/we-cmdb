2b3040bba4bf8669598e5468a656e67e1593b37d
gavin2lee <gavin2lee@163.com>
2021-03-29 17:15:48 +0800

    #1224 modify version to upgrade

--- cmdb-core/src/main/resources/database/ch/05.cmdb.upgrade_ch.sql
+++ cmdb-core/src/main/resources/database/ch/05.cmdb.upgrade_ch.sql
@@ -1,4 +1,4 @@
-#@1.5.9.0-begin@
+#@1.5.8.18-begin@
 ALTER TABLE `adm_ci_type_attr` 
 CHANGE COLUMN `auto_fill_rule` `auto_fill_rule` VARCHAR(3000) NULL DEFAULT NULL;
-#@1.5.9.0-end@
+#@1.5.8.18-end@
