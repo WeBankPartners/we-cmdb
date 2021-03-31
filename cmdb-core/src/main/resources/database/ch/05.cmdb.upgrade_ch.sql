36ae18c44d8d2f3ad2ab21c9a3873b699652f3bd
gavin2lee <gavin2lee@163.com>
2021-03-31 10:08:27 +0800

    #1224 fix version tag issue

--- cmdb-core/src/main/resources/database/ch/05.cmdb.upgrade_ch.sql
+++ cmdb-core/src/main/resources/database/ch/05.cmdb.upgrade_ch.sql
@@ -1,3 +1,3 @@
-#@1.5.8.18-begin@;
+#@v1.5.8.18-begin@;
 ALTER TABLE `adm_ci_type_attr` CHANGE COLUMN `auto_fill_rule` `auto_fill_rule` VARCHAR(3000) NULL DEFAULT NULL;
-#@1.5.8.18-end@;
+#@v1.5.8.18-end@;
