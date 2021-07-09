4678d1c2fadc66f92ce0d2d0befd35aa4da6b202
zgyzgyhero <894526647@qq.com>
2021-07-09 17:18:14 +0800

    update router js

--- cmdb-ui/src/router.js
+++ cmdb-ui/src/router.js
@@ -18,13 +18,6 @@ export default new Router({
           props: true
         },
         {
-          path: '/wecmdb/designing/graph-config',
-          name: 'graph-config',
-          component: () => import('@/pro/graph-config.vue'),
-          params: {},
-          props: true
-        },
-        {
           path: '/wecmdb/designing/data-mgmt-view',
           name: 'graph-management',
           component: () => import('@/pages/designing/graph-management'),
