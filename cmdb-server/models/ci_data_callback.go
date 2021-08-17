8787a84ad6b73fd5962ad5b7207d4393e57921b1
zgyzgyhero <894526647@qq.com>
2021-08-17 16:20:02 +0800

    Add call action

---
+++ cmdb-server/models/ci_data_callback.go
@@ -0,0 +1,73 @@
+package models
+
+import "time"
+
+type SysWecubeProcessTable struct {
+	Guid                  string    `json:"guid"`
+	CiDataGuid            string    `json:"ci_data_guid"`
+	WecubeProcInstanceTmp string    `json:"wecube_proc_instance_tmp"`
+	WecubeProcInstance    string    `json:"wecube_proc_instance"`
+	WecubeProcDefine      string    `json:"wecube_proc_define"`
+	Status                string    `json:"status"`
+	UpdateTime            time.Time `json:"update_time"`
+}
+
+type CoreProcessQueryResponse struct {
+	Status  string                 `json:"status"`
+	Message string                 `json:"message"`
+	Data    []*CodeProcessQueryObj `json:"data"`
+}
+
+type CodeProcessQueryObj struct {
+	ExcludeMode    string `json:"excludeMode"`
+	ProcDefId      string `json:"procDefId"`
+	ProcDefKey     string `json:"procDefKey"`
+	ProcDefName    string `json:"procDefName"`
+	ProcDefVersion string `json:"procDefVersion"`
+	RootEntity     string `json:"rootEntity"`
+	Status         string `json:"status"`
+	CreatedTime    string `json:"createdTime"`
+	Tags           string `json:"tags"`
+}
+
+type CoreProcessRequest struct {
+	EventSeqNo      string `json:"eventSeqNo"`
+	EventType       string `json:"eventType"`
+	SourceSubSystem string `json:"sourceSubSystem"`
+	OperationKey    string `json:"operationKey"`
+	OperationData   string `json:"operationData"`
+	NotifyRequired  string `json:"notifyRequired"`
+	NotifyEndpoint  string `json:"notifyEndpoint"`
+	OperationUser   string `json:"operationUser"`
+	OperationMode   string `json:"operationMode"`
+}
+
+type CoreProcessResult struct {
+	Status  string                `json:"status"`
+	Message string                `json:"message"`
+	Data    CoreProcessResultData `json:"data"`
+}
+
+type CoreProcessResultData struct {
+	ProcInstId        string                   `json:"procInstId"`
+	Status            string                   `json:"status"`
+	TaskNodeInstances []*CoreProcessResultData `json:"taskNodeInstances"`
+}
+
+type CiDataCallbackParam struct {
+	RowGuid       string `json:"rowGuid"`
+	ProcessKey    string `json:"processKey"`
+	ProcessName   string `json:"processName"`
+	OperationUser string `json:"operationUser"`
+}
+
+type CiDataActionQuery struct {
+	Title      []*CiDataActionQueryTitle `json:"title"`
+	Data       []map[string]interface{}  `json:"data"`
+	Selectable bool                      `json:"selectable"`
+}
+
+type CiDataActionQueryTitle struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
