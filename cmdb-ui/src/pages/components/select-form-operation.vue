ef1b0575dfea09608bc5846ae84012c7aaa06b34
pobu168 <244202969@qq.com>
2021-08-24 11:38:14 +0800

    fix select form bug

--- cmdb-ui/src/pages/components/select-form-operation.vue
+++ cmdb-ui/src/pages/components/select-form-operation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class=" ">
-    <Modal v-model="selectFormConfig.isShow" width="900" :title="params.operation">
+    <Modal v-model="selectFormConfig.isShow" width="1000" :title="params.operation">
       <div>
         <Table
           border
@@ -17,6 +17,16 @@
         <Button type="primary" @click="confirmData" :disabled="currentChoose === ''">{{ $t('confirm') }}</Button>
       </div>
     </Modal>
+    <Modal v-model="dataDetail.isShow" :fullscreen="fullscreen" width="800" :mask-closable="false" footer-hide>
+      <p slot="header">
+        <span>{{ $t('details') }}</span>
+        <Icon v-if="!fullscreen" @click="fullscreen = true" class="header-icon" type="ios-expand" />
+        <Icon v-else @click="fullscreen = false" class="header-icon" type="ios-contract" />
+      </p>
+      <div :style="{ overflow: 'auto', 'max-height': fullscreen ? '' : '500px' }">
+        <pre>{{ dataDetail.data }}</pre>
+      </div>
+    </Modal>
   </div>
 </template>
 
@@ -26,6 +36,12 @@ export default {
   name: '',
   data () {
     return {
+      MODALHEIGHT: 500,
+      fullscreen: false,
+      dataDetail: {
+        isShow: false,
+        data: {}
+      },
       params: {},
       selectFormConfig: {
         isShow: false,
@@ -37,6 +53,9 @@ export default {
       currentChoose: ''
     }
   },
+  mounted () {
+    this.MODALHEIGHT = document.body.scrollHeight - 200
+  },
   methods: {
     cancelData () {
       this.selectFormConfig.isShow = false
@@ -46,10 +65,38 @@ export default {
       this.params = params
       const { statusCode, data } = await getSelectFormData(params)
       if (statusCode === 'OK') {
+        data.title.forEach(item => {
+          item.width = 200
+          if (['object', 'multiObject'].includes(item.type)) {
+            item.width = 260
+            item.render = (h, params) => {
+              return (
+                <div>
+                  <div style="display:inline-block;width: 190px;overflow: hidden;text-overflow:ellipsis;white-space: nowrap">
+                    {JSON.stringify(params.row[item.key])}
+                  </div>
+                  {params.row[item.key] !== '' && (
+                    <Button
+                      onClick={() => {
+                        this.showInfo(params.row[item.key])
+                      }}
+                      style="vertical-align: top;"
+                      icon="ios-search"
+                      type="primary"
+                      ghost
+                      size="small"
+                    ></Button>
+                  )}
+                </div>
+              )
+            }
+          }
+        })
+        const finalData = this.managementData(data.title, data.data)
         data.title.unshift({
-          title: '选择',
+          title: this.$t('select_placeholder'),
           key: 'id',
-          width: 70,
+          width: 80,
           align: 'center',
           render: (h, params) => {
             let id = params.row.id
@@ -80,11 +127,34 @@ export default {
         this.selectFormConfig = {
           isShow: true,
           table: {
-            tableData: data.data,
+            tableData: finalData,
             tableColumns: data.title
           }
         }
       }
+    },
+    showInfo (data) {
+      this.dataDetail.data = ''
+      this.dataDetail.isShow = true
+      this.dataDetail.data = data
+    },
+    managementData (title, data) {
+      const titleJson = {}
+      title.forEach(item => {
+        titleJson[item.key] = item.type
+      })
+      const keys = Object.keys(titleJson)
+      data.forEach(d => {
+        keys.forEach(k => {
+          if (titleJson[k] === 'ref') {
+            d[k] = d[k].key_name || ''
+          }
+          if (titleJson[k] === 'multiRef') {
+            d[k] = d[k].map(item => item.key_name).join(',')
+          }
+        })
+      })
+      return data
     },
     async confirmData () {
       const selectedData = this.selectFormConfig.table.tableData.filter(item => item.id === this.currentChoose)
@@ -95,7 +165,11 @@ export default {
           desc: message
         })
         this.selectFormConfig.isShow = false
-        this.$parent.queryCiData()
+        if (this.$parent.queryCiData) {
+          this.$parent.queryCiData()
+        } else {
+          this.$emit('callback')
+        }
       }
       this.currentChoose = ''
     }
