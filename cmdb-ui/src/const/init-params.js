a97b62108992ebb255897d8193548173797476d2
pobu168 <244202969@qq.com>
2023-05-17 15:13:14 +0800

    update: init params

--- cmdb-ui/src/const/init-params.js
+++ cmdb-ui/src/const/init-params.js
@@ -54,6 +54,11 @@ export const INPUT_TYPE_CONFIG = [
     length: 0
   },
   {
+    code: 'longText',
+    value: 'text',
+    length: 0
+  },
+  {
     code: 'diffVariable',
     value: 'text',
     length: 0
